433afdad61e13b78013c64c4a24a16298cbc7c2d
JO:James Owen <james.owen@imperial.ac.uk>
2021-02-18 15:58:23 +0000

    added scripts to run on DiRAC

---
+++ deployer.py
@@ -0,0 +1,43 @@
+### This deploys a parameter study on HPC
+
+import os
+import numpy as np
+
+no_arad = True
+
+Kzz_grid = np.logspace(5.,10.,6)
+Haze_flux_grid = np.logspace(-16.,-10.,7)
+
+
+chdir = os.getcwd()
+
+for i in range(5,np.size(Kzz_grid)):
+    for j in range(6,np.size(Haze_flux_grid)):
+
+        wk_dir = "run_dir%d_%d"%(i,j)
+
+        os.mkdir(wk_dir)
+        os.chdir(wk_dir)
+        os.system("git clone https://github.com/jo276/2Datmosparticle.git")
+        os.chdir("2Datmosparticle")
+        
+        # now re-write runner file
+        f =open("runner_HJ2.py",'r')
+        filedata = f.read()
+        f.close()
+
+        newfiledata = filedata.replace("Haze_flux = 1e-13","Haze_flux = %e" %(Haze_flux_grid[j]))
+        newfiledata = newfiledata.replace("Kzz = 1e6","Kzz = %e" %(Kzz_grid[i]))
+
+        if (no_arad):
+            newfiledata = newfiledata.replace("Arad = True","Arad = False")
+
+        f = open("runner_use.py",'w')
+        f.write(newfiledata)
+        f.close()
+            
+        os.chdir(chdir)
+
+
+
+
---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PBS -N 2DHaze
+#PBS -l walltime=36:00:00
+#PBS -l nodes=1:ppn=36
+#PBS -m n
+#PBS -q dirac25x
+#PBS -A dp100
+
+cd $PBS_O_WORKDIR
+
+python runner_use.py
