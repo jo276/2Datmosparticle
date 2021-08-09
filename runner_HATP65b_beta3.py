2acac5cd87e655bd8ad69eb9d7819e112974381a
JO:James Owen <james.owen@imperial.ac.uk>
2021-08-09 17:54:49 +0100

    updated runners

--- runner_HATP65b_beta3.py
+++ runner_HATP65b_beta3.py
@@ -26,8 +26,8 @@ Rp = 1.39626304e34
 
 
 #### Simulation parameters
-Nsteps = 20000# total number of timesteps to run
-Ndump = 250 # output every this number of timesteps
+Nsteps = 20000001# total number of timesteps to run
+Ndump = 2500 # output every this number of timesteps # output every this number of timesteps
 Nrat = 20 # update radiative transfer this number of time-steps
 Short_Fric = False ## whether to use short friction time approx or not
 
